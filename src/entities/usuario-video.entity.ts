2e28682e288522d08815e113678cdebd5eb4074b
Gabriel Dias <dias.1618@gmail.com>
2020-08-03 21:32:15 -0300

    Nota adicionado

--- src/entities/usuario-video.entity.ts
+++ src/entities/usuario-video.entity.ts
@@ -1,19 +1,23 @@
-import { PrimaryGeneratedColumn, Entity, ManyToOne, BaseEntity } from "typeorm";
+import { PrimaryGeneratedColumn, Entity, ManyToOne, BaseEntity, Column } from "typeorm";
 import { Video } from "./video.entity";
 import { Usuario } from "./usuario.entity";
 
 @Entity()
 export class UsuarioVideo extends BaseEntity{
 
-    constructor(data: {id?:number, usuario?:Usuario, video?:Video}){
+    constructor(data: {id?:number, nota?:number, usuario?:Usuario, video?:Video}){
         super();
         this.id = data && data.id || 0;
+        this.nota = data && data.nota || 0;
         this.usuario = data && data.usuario || undefined;
         this.video = data && data.video || undefined;
     }
 
     @PrimaryGeneratedColumn()
     id:number;
+
+    @Column("integer", {nullable: true})
+    nota:number;
 
     @ManyToOne(type => Usuario, usuario => usuario.usuariosVideos)
     usuario: Usuario;
@@ -24,6 +28,7 @@ export class UsuarioVideo extends BaseEntity{
     toJson():string{
         return `{
             "id": ${this.id},
+            "nota": ${this.nota},
             "usuario": "${this.usuario.toJson()}",
             "video": "${this.video.toJson()}",
         }`
