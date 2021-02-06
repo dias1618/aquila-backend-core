1d3fd8183b1e6d653e322d0c4a957a5853ce08aa
Gabriel Dias <dias.1618@gmail.com>
2021-02-06 16:58:50 -0300

    Update record.service.ts

--- src/services/record.service.ts
+++ src/services/record.service.ts
@@ -10,12 +10,12 @@ export class RecordService{
     ){}
 
     async cadastrarNovasCategorias():Promise<any>{
-        let response = await this.axios.post(`http://aquila-backend-records:3000/repositories/categorias`, null, null);
+        let response = await this.axios.get(`http://aquila-backend-records:3000/repositories/categorias`);
         return response.data;
     }
 
     async cadastrarNovosVideos():Promise<any>{
-        let response = await this.axios.post(`http://aquila-backend-records:3000/repositories/videos`, null, null);
+        let response = await this.axios.get(`http://aquila-backend-records:3000/repositories/videos`);
         return response.data;
     }
 
